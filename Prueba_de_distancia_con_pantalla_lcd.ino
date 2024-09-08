d6f81dd74b551b7f357f9cb75db5f6077b585aec
Federico Giozzi <fedegiozzi@gmail.com>
2024-09-08 20:24:08 -0300

    Update Prueba_de_distancia_con_pantalla_lcd.ino

--- Prueba_de_distancia_con_pantalla_lcd.ino
+++ Prueba_de_distancia_con_pantalla_lcd.ino
@@ -1,90 +1,38 @@
-// Incluimos la libreria para la pantalla LCD
+#define sensor A05
+int led = 10;
+int ledRojo = 9;
 #include <LiquidCrystal.h>
 // Definimos que puertos estamos utilizando
-LiquidCrystal lcd(2,3,4,5,6,7);
-// Declaramos variables
-int TRIG = 9;
-int ECHO = 8;
-int DISTANCIA;
-int DURACION;
-String dis = "DISTANCIA:";
+LiquidCrystal lcd(12,11,5,4,3,2);
+int valorHumedad = 0;
 
 
 void setup() {
-  // Definimos entradas,salidas y iniciamos el puerto Serial
-  pinMode(TRIG,OUTPUT);
-  pinMode(ECHO,INPUT);
-  Serial.begin(9600);
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  lcd.begin(16,2);
+  pinMode(led,OUTPUT);
+  pinMode(ledRojo,OUTPUT);
 }
 
+
 void loop() {
-  // Prendermos el TRIGGER y lo apagamos para que cada 1 milisegundo mande una señal y el ECHO la reciba
-  digitalWrite(TRIG,HIGH);
-  delay(1);
-  digitalWrite(TRIG,LOW);
-  // Definimos que es duracion y lo dividimos entre 58.2 para que de la medidad en cm y que cada 250 milesimas de segundo mande el valor de la distancia actualizado
-  DURACION = pulseIn(ECHO,HIGH);
-  DISTANCIA = DURACION / 58.2;
-  delay(250);
-  // Ponemos en que linea va a estar el mensaje y hacemos que se desplaze desde la derecha a la izquierda y cada 500 milesimas de segundo cambie el valor de la DISTANCIA
+  // put your main code here, to run repeatedly:
+  int valorHumedad = map(analogRead(sensor),0 , 1023 , 100 ,0);
+  if(valorHumedad>50){
+    digitalWrite(led,HIGH);
+    digitalWrite(ledRojo,LOW);
+    }
+    else{
+      digitalWrite(ledRojo,HIGH);
+      digitalWrite(led,LOW);
+    }
+  
   lcd.setCursor(5,0);
-  lcd.print(dis);
-  lcd.print(DISTANCIA);
+  lcd.print("Humedad:");
+  lcd.print(valorHumedad);
+  lcd.setCursor(5,1);
+  lcd.print("Temperatura:");
+  lcd.print(valorHumedad);
   delay(500);
   lcd.scrollDisplayLeft();
-  
-}
-
-//  Separacion de codigos de pantalla lcd y sensores-botones
-
-
-#include "DHT.h"
-
-int BAP = 3;   //Botón Arranque/Parada
-int BR = 4;    //Bomba Riego
-int EV = 5;    //Electroválvula
-int BMM = 6;   //Botón Marcha Motor
-int M = 7;     //Motor
-//int SAB = 8;   //Sensor Agua Bomba
-int SHT = A0;  //Sensor Humedad Tierra
-int STH = 9;   //Sensor Temperatura & Humedad
-
-#define DHTTYPE DHT11
-
-DHT dht(STH,DHTTYPE);
-
-void setup() {
-
-  Serial.begin(9600);
-  dht.begin();
-  
-  pinMode(Br,OUTPUT);
-
-
-}
-
-void loop() {
-  {
-    float h = dht.readHumidity();       //Lee Humedad ambiente
-    float t = dht.readTemperature();    //Lee Temperatura ambiente
-
-    Serial.print("Humedad: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperatura: ");
-    Serial.print(t);}
-    Serial.print(" *C");
-  }
-  {
-  humedad = analogRead(A0);
-
- if(humedad>=721 && humedad<=1024){
-   digitalWrite(Br,LOW);
- }
- else{
-   digitalWrite(Br,HIGH);
- }
- Serial.println(humedad);
- delay(500);
-  }
-}
